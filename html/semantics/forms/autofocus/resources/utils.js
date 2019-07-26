ff9ee203c3f88c049bfedf27d1a4a850d3682d68
Kent Tamura <tkent@chromium.org>
2019-07-26 19:13:13 +0900

    Fix a typo in focusable-area-in-top-document.html, add new test cases

--- html/semantics/forms/autofocus/resources/utils.js
+++ html/semantics/forms/autofocus/resources/utils.js
@@ -1,13 +1,36 @@
 'use strict';
 
-function waitForAnimationFrame() {
+function waitForAnimationFrame(w) {
+  let targetWindow = w || window;
   return new Promise((resolve, reject) => {
-    requestAnimationFrame(resolve);
+    targetWindow.requestAnimationFrame(resolve);
+  });
+}
+
+function waitForEvent(target, type, options) {
+  return new Promise((resolve, reject) => {
+    target.addEventListener(type, resolve, options);
   });
 }
 
 function waitForLoad(target) {
+  return waitForEvent(target, 'load');
+}
+
+// If an element with autofocus is connected to a document and this function
+// is called, the autofocus result is deterministic after returning from the
+// function.
+// Exception: If the document has script-blocking style sheets, this function
+// doesn't work well.
+async function waitUntilStableAutofocusState(w) {
+  let targetWindow = w || window;
+  // Awaiting two animation frames is an easy way to determine autofocus state.
+  await waitForAnimationFrame(targetWindow);
+  await waitForAnimationFrame(targetWindow);
+}
+
+function timeOut(ms) {
   return new Promise((resolve, reject) => {
-    target.addEventListener('load', resolve);
+    setTimeout(resolve, ms);
   });
 }
